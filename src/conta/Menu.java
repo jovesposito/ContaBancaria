7d6e1c94203d2ac31bc75579d10396ceccf5fa7d
jovesposito <jovesposito@outlook.com>
2024-06-12 13:33:24 -0300

    Implementação das classes menu e cores

---
+++ src/conta/Menu.java
@@ -0,0 +1,104 @@
+package conta;
+
+import java.util.Scanner;
+
+import conta.util.Cores;
+
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int opc;
+		
+		String menu = "*****************************************************\n" +
+	              "\n" +
+	              "                BANCO DO BRAZIL COM Z                \n" +
+	              "\n" +
+	              "*****************************************************\n" +
+	              "\n" +
+	              "            1 - Criar Conta                          \n" +
+	              "            2 - Listar todas as Contas               \n" +
+	              "            3 - Buscar Conta por Numero              \n" +
+	              "            4 - Atualizar Dados da Conta             \n" +
+	              "            5 - Apagar Conta                         \n" +
+	              "            6 - Sacar                                \n" +
+	              "            7 - Depositar                            \n" +
+	              "            8 - Transferir valores entre Contas      \n" +
+	              "            9 - Sair                                 \n" +
+	              "\n" +
+	              "*****************************************************\n" +
+	              "\n" +
+	              "Entre com a opção desejada:                          " +
+	              "\n";
+			
+		while (true) {
+			
+			System.out.print(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND + menu + Cores.TEXT_RESET);
+			
+			opc = sc.nextInt();
+			
+
+			if (opc == 9) {
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu futuro começa aqui!");
+				sobre();
+				sc.close();
+				System.exit(0);
+			}
+			
+			switch (opc) {
+			
+			case 1:
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+				break;
+				
+			case 2:
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
+				break;
+			
+			case 3:
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Contar - por número\n\n");
+				break;
+			
+			case 4:
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
+				break;
+				
+			case 5:
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+				break;
+				
+			case 6:
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+				break;
+				
+			case 7:
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+				break;
+				
+			case 8:
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
+				break;
+				
+			default:
+				System.out.println(Cores.TEXT_RED_BOLD + "Opção Inválida!\n\n");
+				break;
+	
+			}
+			
+		}
+	}
+	
+	public static void sobre() {
+		System.out.println("\n*********************************************************\n");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("João Espósito");
+		System.out.println("github.com/jovesposito");
+		System.out.println("\n*********************************************************");
+	}
+
+}
+
+
